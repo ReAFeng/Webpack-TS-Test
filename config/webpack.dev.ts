11d0f1e8d7f0e4259ec57479501a1f61efbaa470
AFeng <952012549@qq.com>
2023-04-26 16:42:55 +0800

    feat:分离webpack-config测试环境下的配置

---
+++ config/webpack.dev.ts
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { Configuration } from "webpack";
+
+export default {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  output: {
+    publicPath: '/'
+  },
+  devServer: {
+    port: 9999,
+    static: {
+      directory: resolve(__dirname, './public/index.html')
+    },
+    hot: true,
+    compress: true,
+    client: {
+      logging: 'info',
+      overlay: {
+        errors: true,
+        warnings: false
+      }
+    }
+  }
+} as Configuration;
