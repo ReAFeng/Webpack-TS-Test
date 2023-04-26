2175485b7d410944cb8c484019e52fe66db67757
AFeng <952012549@qq.com>
2023-04-26 16:48:59 +0800

    feat:添加webpack-config通用配置文件;

---
+++ config/webpack.common.ts
@@ -0,0 +1,47 @@
+import { resolve } from "path";
+import { Configuration } from "webpack";
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import Dotenv from 'dotenv-webpack';
+
+export default {
+  entry: "./src/main.ts",
+  output: {
+    path: resolve(__dirname, "./dist"),
+    filename: 'static/js/app-[contenthash].js',
+    chunkFilename: 'static/js/chunk-[contenhash].js',
+    cssChunkFilename: 'static/css/chunk-[contenthash].css',
+    clean: true
+  },
+  devServer: {
+    port: 9999,
+    static: {
+      directory: resolve(__dirname, './public/index.html')
+    },
+    hot: true,
+    compress: true
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      "@": resolve(__dirname, "./src/")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|m?j)s$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: resolve(__dirname, './public/index.html'),
+      inject: 'body',
+    }),
+    new Dotenv({
+      path: resolve(__dirname, `.env.${process.env.ENV}`)
+    })
+  ]
+} as Configuration;
